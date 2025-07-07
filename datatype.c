626ac81a8bbc83b09c0a1d5db63e3307283181ac
Elias-Malloy <elimalloy@icloud.com>
2025-07-07 14:02:14 -0400

    added typechecking for most binary operations

--- datatype.c
+++ datatype.c
@@ -21,10 +21,10 @@ typed_syntax_tree *typecheck(syntax_tree *tree) {
 	} while (node = node->left);
 }
 
-bool typecheck_expression(syntax_tree *expr) {
+bool typecheck_expression(typed_syntax_tree *expr) {
 	unsigned char type, ltype, valid_typing;
-	typed_syntax_tree *node, lhs, rhs;
-	data_type etype;
+	typed_syntax_tree *node, lhs, rhs, principal_node, auxilary_node;
+	complete_data_type *complete_type;
 	
 	ltype = expr->left->type;
 	// prepass introduce cast to equate to dominant type
@@ -35,21 +35,36 @@ bool typecheck_expression(syntax_tree *expr) {
 		case SYMBOL_EXPRESSION:
 			// unary post
 			// ugh ordering FUCK FUCK FUCK, when/where does the add go????
+			switch(expr->right->type) {
+				// all the suffix ops
+			}
 			break;
 		// unary pre
 		case TERMINAL_INC:
-			
+		/*
+			expr->type = EXPR_ADD;
+			lhs = expr->right;
+			rhs = lr_tree_node_create(EXPR_CONSTANT, NULL, 1);
+			if (rhs == NULL) return false;
+			rhs->base_type.prim_type = PRIM_UINT64;
+			rhs->base_type.indirection = 0;
+			node = expr->right;
+			valid_typing = typecheck_expression(lhs);
+			if (!valid_typing) return false;
+			expr->base_type = node->base_type;
+			expr->left = lhs;
+			expr->right = rhs;
+		*/	
 			node = lr_tree_node_create(EXPR_ADD, 
 		case TERMINAL_DEC:
 		
-		case TERMINAL_
 		case TERMINAL_PLUS:
 		case TERMINAL_MINUS:
 		case TERMINAL_BITWISE_NOT:
+			
 		case TERMINAL_LOGICAL_NOT:
 		case TERMINAL_STAR:
-			data_type = evaluate_expression_type(expr->right);
-			if (data_type.indirection == 0) return 0;
+			
 			
 		case TERMINAL_AND:
 		
@@ -57,6 +72,7 @@ bool typecheck_expression(syntax_tree *expr) {
 		case SYMBOL_CONSTANT:
 		case SYMBOL_VARIABLE:
 		
+		// binary operators
 		case SYMBOL_PARTIAL_MUL_DIV_MOD:
 		case SYMBOL_PARTIAL_ADD_SUB:
 		case SYMBOL_PARTIAL_SHIFT:
@@ -73,21 +89,88 @@ bool typecheck_expression(syntax_tree *expr) {
 			operator = expr->left->right;
 			rhs = expr->right;
 
+			expr->left = lhs;
+			expr->right = rhs;
+
 			valid_typing = typecheck_expression(lhs);
+			valid_typing &= typecheck_expression(rhs);
+			
+			if (lhs->base_type.indirection > rhs->base_type.indirection || (
+				lhs->base_type.indirection == rhs->base_type.indirection &&
+				lhs->base_type.prim_type >= rhs->base_type.prim_type)) {
+				principal_node = lhs;
+				auxilary_node = rhs;
+			} else {
+				principal_node = rhs;
+				auxilary_node = lhs;
+			}
+			expr->base_type = principal_node->base_type;
+			// reject if there was an type error on the left or right hand side
 			if (!valid_typing) return false;
-			valid_typing = typecheck_expression(rhs);
-			if (!valid_typing) return false;
-		
+
+			// reject void/struct valued expressions
+			if (auxilary_node->base_type.prim_type == PRIM_VOID ||
+				principal_node->base_type.indirection == 0 && principal_node->base_type.prim_type == PRIM_STRUCT ||
+				auxilary_node->base_type.indirection  == 0 && auxilary_node->base_type.prim_type  == PRIM_STRUCT)
+				return false;
+
 			switch (operator) {
-				case TERMINAL_STAR: // mul
+				case TERMINAL_PLUS:
+					expr->type = EXPR_ADD;
+					goto reject_pointer_mixed_with_float;
+				case TERMINAL_MINUS:
+					expr->type = EXPR_SUB;
+					goto accept_any;
+    			case TERMINAL_STAR:
 					expr->type = EXPR_MUL;
 					goto accept_integer_or_float;
-				case TERMINAL_DIV:
+    			case TERMINAL_AND:
+					expr->type = EXPR_BIT_AND;
+					goto accept_integer;
+    			case TERMINAL_DIV:
 					expr->type = EXPR_DIV;
 					goto accept_integer_or_float;
-				case TERMINAL_MOD;
+    			case TERMINAL_MOD:
 					expr->type = EXPR_MOD;
 					goto accept_integer;
+    			case TERMINAL_LSHIFT:
+					expr->type = EXPR_LSHIFT;
+					goto accept_integer;
+    			case TERMINAL_RSHIFT:
+					expr->type = EXPR_RSHIFT;
+					goto accept_integer;
+    			case TERMINAL_LESS:
+					expr->type = EXPR_CMP_LESS
+					goto accept_any;
+    			case TERMINAL_LESS_EQUAL:
+					expr->type = EXPR_CMP_LESS_EQUAL;
+					goto reject_pointer_mixed_with_float;
+    			case TERMINAL_GREATER:
+					expr->type = EXPR_CMP_GREATER;
+					goto reject_pointer_mixed_with_float;
+    			case TERMINAL_GREATER_EQUAL:
+					expr->type = EXPR_CMP_GREATER_EQUAL;
+					goto reject_pointer_mixed_with_float;
+    			case TERMINAL_EQUAL_EQUAL:
+					expr->type = EXPR_CMP_EQUAL_EQUAL;
+					goto reject_pointer_mixed_with_float;
+    			case TERMINAL_NOT_EQUAL:
+					expr->type = EXPR_CMP_NOT_EQUAL;
+					goto reject_pointer_mixed_with_float;
+    			case TERMINAL_BITWISE_XOR:
+					expr->type = EXPR_BIT_XOR;
+					goto accept_integer;
+    			case TERMINAL_BITWISE_OR:
+					expr->type = EXPR_BIT_OR;
+					goto accept_integer;
+    			case TERMINAL_LOGICAL_AND:
+					expr->type = EXPR_LOGICAL_AND;
+					goto accept_any;
+    			case TERMINAL_LOGICAL_OR:
+					expr->type = EXPR_LOGICAL_OR;
+					goto accept_any;
+    			case TERMINAL_ASSIGN:
+					// this one is complicated 
 				case TERMINAL_COMMA:
 					expr->type = EXPR_SEQUENCE;
 					expr->base_type = rhs->base_type;
@@ -95,9 +178,34 @@ bool typecheck_expression(syntax_tree *expr) {
 			}
 			break;
 	}
-	accept_integer_or_float:
-		if (lhs->base_type.prim_type == 
-	accept_integer:
+
+accept_integer:
+	if (principal_node->base_type.prim_type == PRIM_FLOAT32 || 
+		principal_node->base_type.prim_type == PRIM_FLOAT64)
+		return false;
+accept_integer_or_float:
+	if (principal_node->base_type.indirection > 0) return false;
+	goto accept_any;
+reject_pointer_mixed_with_float:
+	if (principal_node->base_type.indirection > 0 && 
+		auxilary_node->base_type.indirection == 0 && (
+		auxilary_node->base_type.prim_type == PRIM_FLOAT32 || 
+		auxilary_node->base_type.prim_type == PRIM_FLOAT64))
+		return false;	
+accept_any:	
+	if (lhs->type == rhs->type) return true;
+	
+	complete_type = lr_tree_node_create(EXPR_TYPE, NULL, NULL);
+	if (complete_type == NULL) return false;
+	complete_type->base_type = principal_node->base_type;
+	node = lr_tree_node_create(EXPR_CAST, complete_type, rhs);
+	if (node == NULL) return false;
+	if (auxilary_node->base_type.prim_type == lhs->base_type.prim_type)
+		expr->left = node;
+	else
+		expr->right = node;
+	
+	return true;
 }
 
 bool typecheck_statement(syntax_tree *stmt) {
